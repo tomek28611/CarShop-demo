959758bff8a4383ef8b201d57be327f907a30799
Tom <tomek286@yahoo.com>
2025-01-08 10:18:50 +0100

    Admin. CRUD products

---
+++ MyShop/Areas/Admin/Controllers/CarController.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MyShop.Models;
+
+namespace MyShop.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CarController : Controller
+    {
+        private readonly ShopContext _context;
+
+        public CarController(ShopContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("[area]/cars/{brand?}")]
+        public IActionResult List(string brand = "all")
+        {
+            List<Car> cars;
+
+            if (brand.Equals("all"))
+            {
+                cars = _context.Cars.ToList();
+            }
+            else
+            {
+                cars = _context.Cars.Where(b => b.Brand == brand).ToList();
+            }
+
+            return View(cars);
+        }
+
+        [HttpGet]
+        public IActionResult Add(int? id)
+        {
+            if (id.HasValue)
+            {
+                var car = _context.Cars.Find(id.Value);
+                if (car != null)
+                {
+                    return View("AddEdit", car);
+                }
+            }
+            return View("AddEdit", new Car());
+        }
+
+        [HttpPost]
+        public IActionResult Add(Car car)
+        {
+            //Add operation if id is 0
+
+            if (ModelState.IsValid)
+            {
+                if (car.Id == 0)
+                {
+                    _context.Cars.Add(car);
+                }
+                else
+                {
+                    _context.Cars.Update(car);
+                }
+
+                _context.SaveChanges();
+                return RedirectToAction("List");
+            }
+            return View("AddEdit", car);
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            var car = _context.Cars.Find(id);
+            if (car != null)
+            {
+                return View(car);
+            }
+            return RedirectToAction("List");
+        }
+
+        [HttpPost]
+        public IActionResult DeleteCar(int id)
+        {
+            var car = _context.Cars.Find(id);
+            if (car != null)
+            {
+                _context.Cars.Remove(car);
+                _context.SaveChanges();
+            }
+            return RedirectToAction("List");
+        }
+    }
+}
--- MyShop/Program.cs
+++ MyShop/Program.cs
@@ -26,6 +26,11 @@ app.UseRouting();
 
 app.UseAuthorization();
 
+app.MapAreaControllerRoute(
+    name: "Admin",
+    areaName: "Admin",
+    pattern: "Admin/{controller=Home}/{action=Index}/{id?}");
+
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
